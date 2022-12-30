5cb153ea9bcbad2f399bbeb56224877d88fa2217
yash <yashshekhar2934@gmail.com>
2022-12-31 01:12:44 +0530

    crud operations

---
+++ src/components/Employees.js
@@ -0,0 +1,21 @@
+const Employees=[
+    {
+        id:"1",
+        Name:"Aman Singh",
+        Age:"20"
+    },{
+        id:"2",
+        Name:"Vishal Sharma",
+        Age:"26"
+    },{
+        id:"3",
+        Name:"Manas Verma",
+        Age:"32"
+    },{
+        id:"4",
+        Name:"Rohit Singh",
+        Age:"19"
+    }
+]
+
+export default Employees;
